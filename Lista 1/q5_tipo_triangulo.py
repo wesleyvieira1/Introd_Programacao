634c59b160a731c3d1e22d4cff01dcd9317e2fcf
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2023-08-17 22:19:03 -0300

    Limpeza de Código

--- Lista 1/q5_tipo_triangulo.py
+++ Lista 1/q5_tipo_triangulo.py
@@ -4,7 +4,7 @@ lado_tres = float(input("Digite um valor para um triângulo: "))
 
 if lado_um==lado_dois==lado_tres:
     print("Triângulo Equilátero")
-elif lado_um==lado_dois or lado_dois==lado_tres:
+elif lado_um==lado_dois or lado_dois==lado_tres or lado_um==lado_tres:
     print("Triângulo Isósceles")
 else:
     print("Triângulo Escaleno")
