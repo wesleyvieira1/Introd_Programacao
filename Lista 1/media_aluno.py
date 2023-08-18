7b80443362d34f8064f484bc45c2fea94b180f22
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2023-08-17 22:02:16 -0300

    Limpeza de Código

--- Lista 1/media_aluno.py
+++ Lista 1/media_aluno.py
@@ -3,17 +3,16 @@ nota_dois = float(input("Digite a segunda nota: "))
 
 media = (nota_um+nota_dois)/2
 print(media)
+
 if 9.0 <= media <= 10.0:
-    print("Parabéns! Sua média foi A")
-
+    conceito = "A"
 elif 7.5 <= media <= 9.0:
-    print("Ótimo! Sua média foi B")
-
+    conceito = "B"
 elif 6.5 <= media <= 7.5:
-    print("Boa! Sua média foi C")
-
+    conceito = "C"
 elif 4.0 <= media <= 6.0:
-    print("Estude mais! Sua média foi D")
-
+    conceito = "D"
 else:
-    print("Reprovado! Sua média foi E")
+    conceito = "E"
+
+print(f"O conceito da média do aluno foi {conceito}")
--- Lista 1/salario.py
+++ Lista 1/salario.py
@@ -5,8 +5,8 @@ salario = ganho_por_hora*hora_trabalhada
 imposto_renda = salario*0.11
 inss = imposto_renda*0.08
 sindicato = inss*0.05
-
 salario_liquido = salario - imposto_renda - inss - sindicato
+
 print(f"\nSalário Bruto: R$ {salario:.2f}")
 print(f"IR(11%): R$ {imposto_renda:.2f}")
 print(f"INSS(8%): R$ {inss:.2f}")
