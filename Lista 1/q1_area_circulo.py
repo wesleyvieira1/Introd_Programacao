924c11a8f15fd010c1310ad56cc48c5deffe44a4
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2023-08-17 22:20:40 -0300

    Limpeza de Código

--- Lista 1/q1_area_circulo.py
+++ Lista 1/q1_area_circulo.py
@@ -1,3 +1,3 @@
 raio = float(input("Digite o valor de um raio: "))
 area_circulo = 3.14*raio**2
-print(f'A área do círculo é {area_circulo:.1f}')
+print(f'A área do círculo é {area_circulo:.2f}cm²')
