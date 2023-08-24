851bc8080ee3087edb54d75dca38f0a1bee0d3a0
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2023-08-23 22:01:02 -0300

    Limpeza

--- Lista 1/q2_peso_ideal.py
+++ Lista 1/q2_peso_ideal.py
@@ -3,10 +3,10 @@ genero = int(input("Você é homem ou mulher?\n Digite 1 - Mulher\n Digite 2 - H
 
 if genero==1:
     peso = (72.7*altura) - 58
-    print(f"Seu peso ideal é {peso}")
+    print(f"Seu peso ideal é {peso:.2f}")
 if genero==2:
     peso = (61.1*altura) - 44.7
-    print(f"Seu peso ideal é {peso}")
+    print(f"Seu peso ideal é {peso:.2f}")
 else:
     print("Digitou o número errado!!")
 
